3a96a8a786f9afd936ff3840dbba7d3fb92115c9
Yavuz Selim <yavuz.yesilot@gmail.com>
2021-09-22 23:52:14 +0300

    OOP1 was added.

---
+++ OOP1/ProductManeger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManeger
+    {
+
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " added.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " updated.");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Desk";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 25;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Pen", UnitPrice = 50, UnitsInStock = 100 };
+
+            ProductManeger productManeger = new ProductManeger();
+
+            productManeger.Add(product1);
+
+
+        }
+    }
+}
