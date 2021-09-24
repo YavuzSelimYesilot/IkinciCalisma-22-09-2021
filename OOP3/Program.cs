7bbfc293606f307e1b77fbb2079546e0e2b89905
Yavuz Selim <yavuz.yesilot@gmail.com>
2021-09-24 18:09:13 +0300

    interface intro

---
+++ OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            basvuruManager.BasvuruYap(ihtiyacKrediManager);                                 //1
+            basvuruManager.BasvuruYap(tasitKrediManager);                                   //2
+            basvuruManager.BasvuruYap(konutKrediManager);                                   //3
+
+
+        }
+    }
+}
